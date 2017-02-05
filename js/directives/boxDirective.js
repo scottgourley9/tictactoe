3dcaad4ffaa88537f2b0c316c0642ee87322de1a
Scott Gourley <scottgourley9@gmail.com>
2017-02-05 03:09:03 -0700

    yes yes

--- js/directives/boxDirective.js
+++ js/directives/boxDirective.js
@@ -17,7 +17,22 @@ angular.module('ticTacToe').directive('boxDirective', function(){
     var three;
     var nextMove;
 
-
+    var checkForWinnerComputer = function(movesArr){
+    for(var j = 0; j < winningCombos.length; j++){
+          var count = 0;
+          var val = winningCombos[j]
+          for(var i = 0; i < movesArr.length; i++){
+            if(val.indexOf(movesArr[i]) !== -1){
+              count++
+              if(count === 3){
+                alert('YOU LOSE')
+                return
+              }
+            }
+          }
+      }
+      return
+    }
     var checkForWinner = function(movesArr){
     for(var j = 0; j < winningCombos.length; j++){
           var count = 0;
@@ -32,95 +47,75 @@ angular.module('ticTacToe').directive('boxDirective', function(){
                 winningCombos.splice(j, 1)
                 j--
               }
-              else if(count === 3){
-                three = true
-              }
             }
           }
       }
       return {
-        two: two,
-        three: three
+        two: two
       }
-}
-
-var computerTurn = function(){
-  if(computerMoves.length === userMoves.length){
-    return
-  }
-  if(nextMove && available.indexOf(nextMove) !== -1){
-    $('[boxnum=' + nextMove + ']').css({'background': 'blue'})
-    computerMoves.push(nextMove)
-    available.splice(available.indexOf(nextMove), 1)
-    nextMove = false
-  }
-  else {
-    if(available.length === 8){
-      if(available.indexOf(5) !== -1){
-        $('[boxnum=' + 5 + ']').css({'background': 'blue'})
-        computerMoves.push(5)
-        available.splice(available.indexOf(5), 1)
-      }
-      else {
-        var num = Math.floor(Math.random() * 4)
-        $('[boxnum=' + corners[num] + ']').css({'background': 'blue'})
-        computerMoves.push(corners[num])
-        available.splice(available.indexOf(corners[num]), 1)
-      }
+    }
+  var computerTurn = function(){
+    if(computerMoves.length === userMoves.length){
+      return
+    }
+    if(nextMove && available.indexOf(nextMove) !== -1){
+      $('[boxnum=' + nextMove + ']').css({'background': 'blue'})
+      computerMoves.push(nextMove)
+      available.splice(available.indexOf(nextMove), 1)
+      nextMove = false
     }
     else {
-
-      var p;
-      var k = [];
-      for (var i = 0; i < computerMoves.length; i++) {
-        p = winningCombos.filter(function(v){
-         if(v.indexOf(computerMoves[i]) !== -1 && v.indexOf(userMoves[i]) === -1){
-           k.push(v)
-           return v
-         }
-       })
-      }
-      console.log('k',k);
-      console.log('p', p);
-
-      if(available.length < 3){
-        var lastNum = Math.floor(Math.random() * available.length)
-        $('[boxnum=' + available[lastNum] + ']').css({'background': 'blue'})
-        computerMoves.push(available[lastNum])
-        available.splice(available.indexOf(available[lastNum]), 1)
-        return
-      }
-      var theOne
-      if(!p.length){
-        theOne = k[0]
-      }
-      else{
-        theOne = p[0]
-      }
-      for(var i = 0; i < theOne.length; i++){
-        if(available.indexOf(theOne[i]) !== -1){
-          $('[boxnum=' + theOne[i] + ']').css({'background': 'blue'})
-          computerMoves.push(theOne[i])
-          available.splice(available.indexOf(theOne[i]), 1)
-             var obj = checkForWinner(computerMoves)
-             nextMove = obj.two
-          return
+      if(available.length === 8){
+        if(available.indexOf(5) !== -1){
+          $('[boxnum=' + 5 + ']').css({'background': 'blue'})
+          computerMoves.push(5)
+          available.splice(available.indexOf(5), 1)
+        }
+        else {
+          var num = Math.floor(Math.random() * 4)
+          $('[boxnum=' + corners[num] + ']').css({'background': 'blue'})
+          computerMoves.push(corners[num])
+          available.splice(available.indexOf(corners[num]), 1)
         }
       }
-
-
-
+      else {
+        var p;
+        var k = [];
+        for (var i = 0; i < computerMoves.length; i++) {
+          p = winningCombos.filter(function(v){
+           if(v.indexOf(computerMoves[i]) !== -1 && v.indexOf(userMoves[i]) === -1){
+             k.push(v)
+             return v
+           }
+         })
+        }
+        if(available.length < 3){
+          var lastNum = Math.floor(Math.random() * available.length)
+          $('[boxnum=' + available[lastNum] + ']').css({'background': 'blue'})
+          computerMoves.push(available[lastNum])
+          available.splice(available.indexOf(available[lastNum]), 1)
+          return
+        }
+        var theOne
+        if(!p.length){
+          theOne = k[0]
+        }
+        else{
+          theOne = p[0]
+        }
+        for(var i = 0; i < theOne.length; i++){
+          if(available.indexOf(theOne[i]) !== -1){
+            $('[boxnum=' + theOne[i] + ']').css({'background': 'blue'})
+            computerMoves.push(theOne[i])
+            available.splice(available.indexOf(theOne[i]), 1)
+               var obj = checkForWinner(computerMoves)
+               nextMove = obj.two
+            return
+          }
+        }
+      }
     }
   }
-
-  //find first possible winner for computer
-// console.log('this is happening');
-//    var obj = checkForWinner(computerMoves)
-//    nextMove = obj.two
-//    console.log('nxt', nextMove);
-  //check for two
-}
-
   return {
     restrict: 'A',
     link: function(scope, element, attrs){
@@ -134,7 +129,7 @@ var computerTurn = function(){
           return
         }
         var checkObject = checkForWinner(userMoves)
-        if(checkObject.two && computerMoves.indexOf(checkObject.two) === -1 && !nextMove){
+        if(checkObject.two && computerMoves.indexOf(checkObject.two) === -1 && available.indexOf(nextMove) === -1){
           //check to see which number box I need to block
           $('[boxnum=' + checkObject.two + ']').css({'background': 'blue'})
           computerMoves.push(checkObject.two)
@@ -142,13 +137,10 @@ var computerTurn = function(){
           checkObject.two = false;
           var obj = checkForWinner(computerMoves)
           nextMove = obj.two
-
         }
         computerTurn()
-
-      }
-
-    })
+        }
+      })
+    }
   }
-}
 })
