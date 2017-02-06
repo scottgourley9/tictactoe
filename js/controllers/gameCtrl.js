e769d76d6f1e50d43b92eeb61ce46d1a8d15741c
Scott Gourley <scottgourley9@gmail.com>
2017-02-06 02:10:55 -0700

    all done

--- js/controllers/gameCtrl.js
+++ js/controllers/gameCtrl.js
@@ -2,4 +2,7 @@ angular.module('ticTacToe').controller('gameCtrl', function($scope){
   $scope.reset = function(){
     window.location.reload(true);
   }
+  var num = Math.floor(Math.random() * 6)
+  var colors = ['red', 'yellow', 'orange', 'purple', 'blue', 'green']
+  $scope.color = colors[num]
 })
